a254fe13ccaa0a6d1c8b5190e460116a32146369
xiongjie.xue <xiongjie.xue@lkcoffee.com>
2022-02-08 12:03:53 +0800

    fix: 🐛 fix "module" or an .mjs extension for the entry point

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 import { uglify } from "rollup-plugin-uglify";
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs'
 import path from 'path'
+
+import packageJSON from './package.json'
 
 const extensions = [
   '.js',
@@ -13,12 +16,29 @@ const getPath = _path => path.resolve(__dirname, _path)
 export default {
   input: "src/index.ts",
   external: ['vuex'],
-  output: {
-    file: "dist/index.js",
-    format: "es",
-  },
+  output: [
+    {
+      file: packageJSON.main, // commonjs模块
+      format: 'cjs',
+      name: packageJSON.name,
+    },
+    {
+      file: packageJSON.browser, // 通用模块
+      format: 'umd',
+      name: packageJSON.name,
+      globals: {
+        vuex: 'vuex'
+      }
+    },
+    {
+      name: packageJSON.name,
+      file: packageJSON.module, // es模块
+      format: "es",
+    }
+  ],
   plugins: [
     resolve(extensions),
+    commonjs(),
     uglify(),
     typescript({
       useTsconfigDeclarationDir: true,
